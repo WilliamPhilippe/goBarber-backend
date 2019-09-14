e5cab009dd1cd19a9ccc362c5e732cbc9b43d542
William Philippe <wplb@ic.ufal.br>
2019-09-14 05:27:41 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '12121212',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  res.json(user);
-});
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
