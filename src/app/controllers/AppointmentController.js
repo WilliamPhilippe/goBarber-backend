bc03c6396a7a23a6c64c5ea8cba6338b78e13f30
William Philippe <wplb@ic.ufal.br>
2019-09-20 20:16:48 -0300

    finalizado

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, cancelled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancellable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
