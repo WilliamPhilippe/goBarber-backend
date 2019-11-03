b72c05d62f9981f95dd7769af442f02cbafaa008
William Philippe <wplb@ic.ufal.br>
2019-11-03 08:01:16 -0300

    update

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -15,7 +15,7 @@ class SessionController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails.' });
+      return res.status(400).json({ error: 'Validation fails at yup.' });
     }
 
     const { email, password } = req.body;
